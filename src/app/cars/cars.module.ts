fef19f2ec840e402995835f1f15bd6a9d73c4e85
Artur Górny <agorny112@gmail.com>
2022-06-23 16:55:32 +0200

    total cost + gross cost

---
+++ src/app/cars/cars.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarsListComponent } from './cars-list/cars-list.component';
+import { TotalCostComponent } from './total-cost/total-cost.component';
+
+
+
+@NgModule({
+  declarations: [
+    CarsListComponent,
+    TotalCostComponent
+  ],
+  exports: [
+    CarsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarsModule { }
---
+++ src/app/cars/total-cost/total-cost.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'cs-total-cost',
+  templateUrl: './total-cost.component.html',
+  styleUrls: ['./total-cost.component.less']
+})
+export class TotalCostComponent {
+
+  private VAT : number = 1.23;
+
+  @Input()
+  totalCost!: any; // checking problem w/o initializing field with type number.
+
+  @Output()
+  shownGross : EventEmitter<number> = new EventEmitter<number>();
+
+  showGross(): void {
+    this.shownGross.emit(this.totalCost * this.VAT)
+  }
+}
