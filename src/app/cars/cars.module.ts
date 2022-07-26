0c3c8cf12717001c25c6fd565d7a260d8a39241e
Artur Górny <agorny112@gmail.com>
2022-07-26 23:41:26 +0200

    header + fixing total price

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarsListComponent } from './cars-list/cars-list.component';
 import { TotalCostComponent } from './total-cost/total-cost.component';
+import {SharedModule} from "../shared-module/shared.module";
 
 
 
@@ -14,7 +15,8 @@ import { TotalCostComponent } from './total-cost/total-cost.component';
     CarsListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class CarsModule { }
